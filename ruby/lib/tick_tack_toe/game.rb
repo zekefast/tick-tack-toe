abdfe782715dc0105e0909acff5b7e722e29ea27
Zeke Fast <zekefast@gmail.com>
2023-11-17 01:06:04 +0100

    Implement main logic of the game in ::TickTackToe::Game class.
    Changes:
    - Check for winner or draw in ::TickTackToe::Game.move method.
      The draw check required intorduction of move counter.
    - Introduce ::TickTackToe::Game.positions method to generate
      Enumerator-s of Position-s. These Enumerator-s is used to build
      lines which has to be check to determine the winner.
    - Add the following methods to ::TickTackToe::Game class to depermine
      position belonings to diagonals:
      - belongs_to_minor_diagonal?
      - belongs_to_major_diagonal?
    - Introduce ::TickTackToe::Game.checked_lines method to generate lines
      (row, column or diagonals) which has to be checked in order to
      determine the winner or a draw.
      The implementation is heavily relies on ::TickTackToe::Game.positions
      method and describe lines in terms of Range-s and .positions method
      transform these Range-s to Enumerator-s of Position-s.
    - Introduce ::TickTackToe::Game.final_move? method to determine a draw.
      It receives counter which starts from 0.
      So, for standard game filed 3x3 there should be 9 moves in total which
      counts from 0 till 8.
    - Introduce ::TickTackToe::Game.won? which checks whether the player
      who recently made a move won the game.
      The implementation relies on lines generation made by earlier
      described checked_lines method.
      won? method essentially checks whether there is any line with marks of
      the player who made the move in all positions of the line.

--- ruby/lib/tick_tack_toe/game.rb
+++ ruby/lib/tick_tack_toe/game.rb
@@ -19,13 +19,111 @@ class ::TickTackToe::Game
     @players = players
   end
 
-  # @return [::TickTackToe::Player] winner of the game
+  # @return [::TickTackToe::Player, nil] winner of the game, otherwise - nil
+  #   (draw game).
   #
   # @yieldparam player [::TickTackToe::Player]
   # @yieldreturn [::TickTackToe::Position]
   def move
-    players.cycle do |player|
-      @board.set(yield(player), player.mark)
+    players.cycle.each_with_index do |player, move_counter|
+      position = yield(player)
+      @board.set(position, player.mark)
+
+      if won?(player, position)
+        return player
+      elsif final_move?(move_counter)
+        return nil
+      end
+    end
+  end
+
+  # @param player     [::TickTackToe::Player]
+  # @param position   [::TickTackToe::Position]
+  #
+  # @return [Boolean]
+  def won?(player, position)
+    checked_lines(position).any? do |line|
+      line.all? do |position|
+        @board.get(position) == player.mark
+      end
+    end
+  end
+
+  # @param counter [Integer]
+  #
+  # @return [Boolean]
+  def final_move?(counter)
+    counter >= (@board.size**2 - 1)
+  end
+
+  # GOTCHA: As we have to use reverse range for calculation of right top to
+  #   left bottom diagonal in #checked_lines method and Ruby for some reason
+  #   does not handle reverse ranges properly we refine Ruby behaviour
+  #   regarding Ranges.
+  using ::TickTackToe::Util::ReverseRange
+
+  def checked_lines(position)
+    lines = [
+      # Check vertical containing a cell with recent move of the player
+      positions(position.x, 0...@board.size),
+      # Check horizontal containing a cel with recent move of the player
+      positions(0...@board.size, position.y),
+    ]
+
+    # Check top left to right bottom diagonal only if there were a move which
+    # belongs to it.
+    lines << positions(0...@board.size, 0...@board.size) if belongs_to_major_diagonal?(position)
+    # Check top right to left bottom diagonal only if there were a move which
+    # belongs to it.
+    lines << positions(@board.size.pred..0, 0...@board.size) if belongs_to_minor_diagonal?(position)
+
+    lines
+  end
+
+
+  # Check whether position belongs to top left to right bottom diagonal.
+  #
+  # @param position [::TickTackToe::Position]
+  #
+  # @return [Boolean]
+  def belongs_to_major_diagonal?(position)
+    position.x == position.y
+  end
+
+  # Check whether position belongs to top right to left bottom diagonal.
+  #
+  # @param position [::TickTackToe::Position]
+  #
+  # @return [Boolean]
+  def belongs_to_minor_diagonal?(position)
+    position.x + position.y == @board.size.pred
+  end
+
+  # @param x [Fixnum, Range]
+  # @param y [Fixnum, Range]
+  #
+  # @return [Enumerator<::TickTackToe::Position>]
+  def positions(x, y)
+    if x.is_a?(Integer) && y.is_a?(Integer)
+      # GOTCHA: as #positions method is intended to generate lines this
+      #   particular branch does not used, but put here for unified behaviour
+      #   of the method.
+      return Enumerator.new { |yielder| yielder << ::TickTackToe::Position.new(x, y) }
+    end
+
+    if x.is_a?(Integer)
+      Enumerator.new do |yielder|
+        y.each { |j| yielder << ::TickTackToe::Position.new(x, j) }
+      end
+    elsif y.is_a?(Integer)
+      Enumerator.new do |yielder|
+        x.each { |i| yielder << ::TickTackToe::Position.new(i, y) }
+      end
+    else
+      j = y.each
+      Enumerator.new do |yielder|
+        x.each { |i| yielder << ::TickTackToe::Position.new(i, j.next) }
+      end
     end
   end
 end
